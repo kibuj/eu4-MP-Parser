73484e416dea3a9894339fb6b3aba1e837960018
bogdan <bogdankibuj@gmail.com>
2025-04-22 16:13:01 +0300

    add remove_TAG def and fix bug

--- main.py
+++ main.py
@@ -14,9 +14,7 @@ def parser(filename):
 
                 block.append(line)
     block.pop(0)
-
     return block
-
 
 def country_list(block):
     country = []
@@ -25,16 +23,21 @@ def country_list(block):
             continue
         if values == '"---"':
             continue
+        if values in country:
+            continue
         else:
             country.append(values)
     return country
 
-
-def finally_command(country, remove_list):
+def country_remove_list(country):
+    remove_list = ['"CAS"','"HUN"']
+    alive_country = []
     for i in country:
-        pass
-    print(country)
-    #country.remove('"CAS"')
+        if i not in remove_list:
+            alive_country.append(i)
+    return alive_country
+
+def finally_command(country):
     command = ''
     for i in country:
         for j in country:
@@ -48,8 +51,10 @@ def finally_command(country, remove_list):
 def main():
     penis = parser('TEST.eu4')
     country = country_list(penis)
-    command = finally_command(country,None)
-    #print(command)
-    #print(len(command))
+    #print(country)
+    alive_country = country_remove_list(country)
+    command = finally_command(alive_country)
+    print(command)
+    print(len(command))
 
 main()
